8f8f96f8e41accb53db44a47ca6f1102137f8add
kan555 <kan-sign-in@Hotmail.com>
2016-01-09 14:58:52 +0700

    bind widgets

--- app/src/main/java/com/donedream/sillapajarn/chawametha/trafficsigns/MainActivity.java
+++ app/src/main/java/com/donedream/sillapajarn/chawametha/trafficsigns/MainActivity.java
@@ -1,13 +1,50 @@
 package com.donedream.sillapajarn.chawametha.trafficsigns;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //explicit การประกาศตัวแปล
+    //access type    name
+    private ListView trafficListView;
+    private Button aboutmeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind widget ผูกตัวแปลกับ widget ใน
+        bindWidget();
+
+        //button controller
+        buttonController();
+
+    }   //Main Method [start operation use onCreate always]
+
+    private void buttonController() {
+
+        //class object = new class();
+        aboutmeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //sound effect
+                MediaPlayer buttomMediaPlayer = MediaPlayer.create(getBaseContext(),
+                        R.raw.end_game);
+                buttomMediaPlayer.start();
+            }   // event
+        });
+
+    }   //buttonController
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView); //alt+enter => cast to data type listview
+        aboutmeButton = (Button) findViewById(R.id.button);
     }
-}
+}   //Main Class
